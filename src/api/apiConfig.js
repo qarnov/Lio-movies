a09749af785cbd06a8b5d7bdce8e83b4c900a40b
Afdhal <afdhalrahman26@gmail.com>
2023-05-10 00:37:11 +0530

    api

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,8 +1,9 @@
 const apiConfig = {
   baseUrl: "https://api.themoviedb.org/3/",
-  apiKey: `91665036ffa5773bb8fe907fe0a3511f`,
+  apiKey:  process.env.REACT_APP_API_SECRET,
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
 
 export default apiConfig;
+
